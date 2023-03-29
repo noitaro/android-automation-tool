846082969538981a3d4533e550910f035adef10a
Your Name <you@example.com>
2023-03-29 19:20:26 +0900

    aaa

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,7 +10,7 @@ use std::fs;
 use std::fs::File;
 use std::path::Path;
 use serde_json::json;
-use opencv::{core, highgui, imgproc};
+use opencv::{imgproc, imgcodecs, core};
 
 #[tauri::command]
 fn my_custom_command1() {
@@ -134,27 +134,24 @@ async fn img_get_file_src_command(file_name: String) -> Result<String, String> {
 async fn adb_touchscreen_img_command(adb: String, img_path: String) -> Result<bool, String> {
 
   // テンプレートマッチングを実行するための画像とテンプレート画像を読み込みます。
-  let image = highgui::imread("path/to/image.jpg", highgui::IMREAD_COLOR)?;
-  let template = highgui::imread("path/to/template.jpg", highgui::IMREAD_COLOR)?;
+  let image = imgcodecs::imread("./image.png", imgcodecs::IMREAD_COLOR).unwrap();
+  let template = imgcodecs::imread("./template.png", imgcodecs::IMREAD_COLOR).unwrap();
 
   // 画像のグレースケール化を行います。
-  let mut gray_image = Mat::default();
-  imgproc::cvt_color(&image, &mut gray_image, imgproc::COLOR_BGR2GRAY, 0)?;
-  let mut gray_template = Mat::default();
-  imgproc::cvt_color(&template, &mut gray_template, imgproc::COLOR_BGR2GRAY, 0)?;
+  let mut gray_image = core::Mat::default();
+  imgproc::cvt_color(&image, &mut gray_image, imgproc::COLOR_BGR2GRAY, 0).unwrap();
+  let mut gray_template = core::Mat::default();
+  imgproc::cvt_color(&template, &mut gray_template, imgproc::COLOR_BGR2GRAY, 0).unwrap();
 
   // テンプレートマッチングを実行します。
-  let mut result = Mat::default();
-  imgproc::match_template(&gray_image, &gray_template, &mut result, imgproc::TM_CCOEFF_NORMED, None)?;
-  // imgproc::match_template(&gray_image, &gray_template, &mut result, opencv::imgproc::TM_CCORR_NORMED, None)?;
+  let mut result = core::Mat::default();
+  imgproc::match_template(&gray_image, &gray_template, &mut result, imgproc::TM_CCOEFF_NORMED, &core::no_array()).unwrap();
 
   // テンプレートマッチングの結果を取得します。
-  let min_val = 0.9; // 閾値
-  let mut max_val = 0.0;
-  let mut max_loc = Point::new(0, 0);
-  result.min_max_loc(Some(&mut min_val), Some(&mut max_val), Some(&mut Point::new(0, 0)), Some(&mut max_loc), None);
+  let mut min_val = 0.9; // 閾値
+  let mut max_loc = core::Point::new(0, 0);
+  core::min_max_loc(&result, Some(&mut min_val), None, None, Some(&mut max_loc), &core::no_array()).unwrap();
   println!("Max Location: {:?}", max_loc);
-
 
   Ok(false)
 }
